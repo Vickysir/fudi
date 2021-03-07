ff58b485bf8d68fe24ebf53958e757c02016a2d9
Vickysir <460022058@qq.com>
2021-03-07 20:34:41 +0800

    add(personalCenter):myOders css

--- src/pages/web/personalCenter/myOrders/index.tsx
+++ src/pages/web/personalCenter/myOrders/index.tsx
@@ -18,33 +18,33 @@ const MyOrders = () => {
             <div>
                 <h3>Active</h3>
                 <div className="myOrders-wrap-list">
-                    <Card
-                        title="Jungle Pizza · Delivery · 3Item · €71"
-                        bordered={false}
-                        extra={<InfoCircleOutlined />}
-                    >
-                        <div className="myOrders-wrap-list-content">
-                            <p>Estimated Time:<span><HistoryOutlined /> 18:00</span></p>
-                            <p>Status:<span>Delivering</span></p>
-                        </div>
-                        <div className="myOrders-wrap-list-footer">Order #3235</div>
-                    </Card>
+                    <ul className="myOrders-wrap-list-title">
+                        <li>Jungle Pizza <span>·</span> Delivery <span>·</span> 3Item <span>·</span> €71</li>
+                        <li><InfoCircleOutlined /></li>
+                    </ul>
+                    <div className="myOrders-wrap-list-content">
+                        <p>Estimated Time:<span><HistoryOutlined /> 18:00</span></p>
+                        <p>Status:<span>Delivering</span></p>
+                    </div>
+                    <div className="myOrders-wrap-list-footer">Order #3235</div>
                 </div>
-
             </div>
             <div>
                 <h3>Completed</h3>
                 <div className="myOrders-wrap-list">
-                    <Card
-                        title="Jungle Pizza · Delivery · 3Item · €71"
-                        bordered={false}
-                        extra={<InfoCircleOutlined />}
-                    >
-                        <p>Card content</p>
-                        <Button type="primary" block>Order #3235</Button>
-                    </Card>
+                    <ul className="myOrders-wrap-list-title title-completed">
+                        <li>Jungle Pizza <span>·</span> Delivery <span>·</span> 3Item <span>·</span> €71</li>
+                        <li><InfoCircleOutlined /></li>
+                    </ul>
+                    <div className="myOrders-wrap-list-content">
+                        <p>Estimated Time:<span className="completed"><HistoryOutlined /> 18:00</span></p>
+                        <p>Status:<span className="completed">Recieved</span></p>
+                    </div>
+                    <ul className="myOrders-wrap-list-footer footer-completed">
+                        <li>Order #3235</li>
+                        <li>Add Review</li>
+                    </ul>
                 </div>
-
             </div>
         </div>
     )

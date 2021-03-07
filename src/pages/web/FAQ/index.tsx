69c671ac378c56d09cb3fdf87e14f217ca1dc43d
Vickysir <460022058@qq.com>
2021-03-07 14:10:05 +0800

    add(homepage):footer

--- src/pages/web/FAQ/index.tsx
+++ src/pages/web/FAQ/index.tsx
@@ -8,6 +8,10 @@
  */
 import React from 'react'
 import { Button, Collapse } from 'antd'
+import WebFooter from '@/pages/components/header/webFooter';
+import WebHeader from '@/pages/components/header/webHeader';
+import BaackTitle from '../components/baackTitle'
+import './index.min.css'
 
 const { Panel } = Collapse;
 const text = `
@@ -17,39 +21,44 @@ const text = `
 `;
 const FAQ = () => {
     return (
-        <div>
-            <h1>Frequently Asked Questions</h1>
-            <Button type="primary">Back</Button>
-            <Collapse accordion>
-                <Panel header="How does FUDI & MORE work?" key="1">
-                    <p>{text}</p>
-                </Panel>
-                <Panel header="What time is FUDI open from?" key="2">
-                    <p>{text}</p>
-                </Panel>
-                <Panel header="What if I have food allergies and/or want to change the order? " key="3">
-                    <p>{text}</p>
-                </Panel>
-                <Panel header="How much does the delivery fee cost?" key="4">
-                    <p>Each delivery is €2 within 3km, additional 50 cents for every 500m beyond 3km.</p>
-                </Panel>
-                <Panel header="How long will my order take to get here?" key="5">
-                    <p>{text}</p>
-                </Panel>
-                <Panel header="Where can I collect my food?" key="6">
-                    <p>{text}</p>
-                </Panel>
-                <Panel header="What is the cancellation policy?" key="7">
-                    <p>{text}</p>
-                </Panel>
-                <Panel header="How can I get free food?" key="8">
-                    <p>{text}</p>
-                </Panel>
-                <Panel header="Can I tip the driver?" key="9">
-                    <p>{text}</p>
-                </Panel>
-            </Collapse>
-        </div>
+        <>
+            <WebHeader />
+            <div>
+                <BaackTitle titleContent="Frequently Asked Questions" />
+                <div className="login-wrap">
+                    <Collapse accordion ghost expandIconPosition="right">
+                        <Panel header="How does FUDI & MORE work?" key="1" className="faq-title">
+                            <p className="faq-desc">{text}</p>
+                        </Panel>
+                        <Panel header="What time is FUDI open from?" key="2" className="faq-title">
+                            <p className="faq-desc">{text}</p>
+                        </Panel>
+                        <Panel header="What if I have food allergies and/or want to change the order? " key="3" className="faq-title">
+                            <p className="faq-desc">{text}</p>
+                        </Panel>
+                        <Panel header="How much does the delivery fee cost?" key="4" className="faq-title">
+                            <p className="faq-desc">Each delivery is €2 within 3km, additional 50 cents for every 500m beyond 3km.</p>
+                        </Panel>
+                        <Panel header="How long will my order take to get here?" key="5" className="faq-title">
+                            <p className="faq-desc">{text}</p>
+                        </Panel>
+                        <Panel header="Where can I collect my food?" key="6" className="faq-title">
+                            <p className="faq-desc">{text}</p>
+                        </Panel>
+                        <Panel header="What is the cancellation policy?" key="7" className="faq-title">
+                            <p className="faq-desc">{text}</p>
+                        </Panel>
+                        <Panel header="How can I get free food?" key="8" className="faq-title">
+                            <p className="faq-desc">{text}</p>
+                        </Panel>
+                        <Panel header="Can I tip the driver?" key="9" className="faq-title">
+                            <p className="faq-desc">{text}</p>
+                        </Panel>
+                    </Collapse>
+                </div>
+            </div>
+            <WebFooter />
+        </>
     )
 }
 

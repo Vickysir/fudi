3d31189d843dde8b02f2e18e5831cabf071228bd
Vickysir <460022058@qq.com>
2021-03-17 18:20:49 +0800

    add(personalCenter):通知中心

--- src/pages/web/personalCenter/notification/index.tsx
+++ src/pages/web/personalCenter/notification/index.tsx
@@ -1,17 +1,35 @@
 /*
  * @Author: your name
  * @Date: 2021-03-04 17:53:51
- * @LastEditTime: 2021-03-04 18:04:05
+ * @LastEditTime: 2021-03-17 15:45:00
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /fudi/src/pages/web/personalCenter/sendQ/index.tsx
  */
 import React from 'react'
+import { DeleteFilled } from '@ant-design/icons';
+import './index.less'
 
 const NotificationCenter = () => {
     return (
-        <div>
-            通知中心
+        <div className="notificationCenter-wrap">
+            <div>
+                <h3>New</h3>
+                <div>
+                    <div className="notificationCenter-wrap-img"></div>
+                    <ul>
+                        <li>
+                            Today <span><DeleteFilled /></span>
+                        </li>
+                        <li>
+                            <h3>Tropical Summer</h3>
+                        </li>
+                        <li>
+                            <p>Fresh discount for the hot summer!All lemonades with -50% discount.</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }

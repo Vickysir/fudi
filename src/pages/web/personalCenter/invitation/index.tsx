3226547304e4cb54c35a21a633352a8460b1c4a3
Vickysir <460022058@qq.com>
2021-03-17 10:31:30 +0800

    add(personalCenter):邀请朋友 css

--- src/pages/web/personalCenter/invitation/index.tsx
+++ src/pages/web/personalCenter/invitation/index.tsx
@@ -1,17 +1,53 @@
 /*
  * @Author: your name
  * @Date: 2021-03-04 17:53:51
- * @LastEditTime: 2021-03-16 16:26:47
+ * @LastEditTime: 2021-03-17 10:28:56
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /fudi/src/pages/web/personalCenter/sendQ/index.tsx
  */
 import React from 'react'
+import { Button } from 'antd'
+import fb from '@/assets/images/common/login/fb.png'
+import './index.less'
 
 const Invitation = () => {
     return (
-        <div className="vouchers-wrap">
-            邀请
+        <div className="invitation-wrap">
+            <header>
+                <h3>Get 50% OFF Your Order</h3>
+                <p>Invite your friends to Fudi&More, and for each one who signs up we’ll give you both 50% discount off next order!</p>
+            </header>
+            <div className="invitation-wrap-code">
+                <div>
+                    Invite Code:
+                    <span>652a</span>
+                </div>
+                <div>
+                    <Button type="primary" shape="round">Share Code</Button>
+                    <img src={fb} alt="icon" />
+                </div>
+            </div>
+            <ul>
+                <li>
+                    <h3>Invitations</h3>
+                    <p>5 Friends</p>
+                </li>
+                <li className="invitation-wrap-friends">
+                    <div>
+                        <img src={fb} alt="icon" />
+                        <h3>Opi Watihana</h3>
+                    </div>
+                    <p className="complete">Complete</p>
+                </li>
+                <li className="invitation-wrap-friends">
+                    <div>
+                        <img src={fb} alt="icon" />
+                        <h3>Opi Watihana</h3>
+                    </div>
+                    <p className="invited">Complete</p>
+                </li>
+            </ul>
         </div>
     )
 }

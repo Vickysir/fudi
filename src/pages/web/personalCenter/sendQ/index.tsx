7d5928708b9e19b5f7b28d0ecf5859930ec86b2e
Vickysir <460022058@qq.com>
2021-03-17 10:59:38 +0800

    add(personalCenter):发送建议 css

--- src/pages/web/personalCenter/sendQ/index.tsx
+++ src/pages/web/personalCenter/sendQ/index.tsx
@@ -1,18 +1,36 @@
 /*
  * @Author: your name
  * @Date: 2021-03-04 17:53:51
- * @LastEditTime: 2021-03-04 17:58:02
+ * @LastEditTime: 2021-03-17 10:58:52
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /fudi/src/pages/web/personalCenter/sendQ/index.tsx
  */
 import React from 'react'
+import { Button, Input } from 'antd'
+import './index.less'
 
+const { TextArea } = Input;
 const SendQ = () => {
+
+    const onChange = e => {
+        console.log(e);
+    };
+
     return (
-        <div>
-            发送建议
-        </div>
+        <div className="sendQ-wrap">
+            <h3>Send Suggestion</h3>
+            <TextArea
+                placeholder="Your Suggestion"
+                allowClear
+                onChange={onChange}
+                autoSize={{ minRows: 6 }}
+            />
+            <div>
+                <Button type="primary" shape="round" style={{ marginRight: "1rem" }}>Cancel</Button>
+                <Button type="primary" shape="round">Send</Button>
+            </div>
+        </div >
     )
 }
 

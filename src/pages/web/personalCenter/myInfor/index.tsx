32336fa2c0f849e4423bc25a26bc451b8a93a05d
Vickysir <460022058@qq.com>
2021-03-19 10:02:16 +0800

    add(personalCenter):修改个人资料

--- src/pages/web/personalCenter/myInfor/index.tsx
+++ src/pages/web/personalCenter/myInfor/index.tsx
@@ -1,17 +1,116 @@
 /*
  * @Author: your name
- * @Date: 2021-03-04 17:53:51
- * @LastEditTime: 2021-03-04 17:56:11
+ * @Date: 2021-03-15 09:12:17
+ * @LastEditTime: 2021-03-18 15:08:00
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: /fudi/src/pages/web/personalCenter/sendQ/index.tsx
+ * @FilePath: /fudi/src/pages/web/personalCenter/myInfor/index.tsx
  */
 import React from 'react'
+import { MailOutlined, UserOutlined, ShakeOutlined, CreditCardOutlined, EnvironmentOutlined, HomeOutlined } from '@ant-design/icons';
+import { Button, Form, Input } from 'antd';
+import './index.less';
 
 const MyInfor = () => {
+
+    const onFinish = (values: any) => {
+        console.log('Received values of form: ', values);
+    };
+
     return (
-        <div>
-            my info
+        <div className="myInfor-wrap">
+            <Form
+                name="normal_login"
+                className="login-form"
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+            >
+                <Form.Item>
+                    <h3 className="myInfor-wrap-title">Edit My Info</h3>
+                </Form.Item>
+                <Form.Item
+                    name="username"
+                    rules={[{ required: true, message: 'Please input your Old Password!' }]}
+                >
+                    <Input
+                        size="large"
+                        prefix={<UserOutlined className="site-form-item-icon" style={{ "margin": "0 1rem" }} />}
+                        placeholder="Username"
+                        style={{ "borderRadius": "5rem", "margin": "0.5rem 0" }}
+                    />
+                </Form.Item>
+                <Form.Item
+                    name="telphone"
+                    rules={[{ required: true, message: 'Please input your New Password!' }]}
+                >
+                    <Input
+                        prefix={<ShakeOutlined className="site-form-item-icon" style={{ "margin": "0 0 0 1rem" }} />}
+                        placeholder="+31 000 000 00 00"
+                        size="large"
+                        style={{ "borderRadius": "5rem", "margin": "0.5rem 0" }}
+                    />
+                </Form.Item>
+                <Form.Item
+                    name="email"
+                    rules={[{ required: true, message: 'Please input your Confirm Password!' }]}
+                >
+                    <Input
+                        prefix={<MailOutlined style={{ "margin": "0 1rem" }} />}
+                        placeholder="Eamil"
+                        size="large"
+                        style={{ "borderRadius": "5rem", "margin": "0.5rem 0" }}
+                    />
+                </Form.Item>
+                <Form.Item>
+                    <h3 className="myInfor-wrap-title">Edit My Address</h3>
+                </Form.Item>
+                <Form.Item
+                    name="location"
+                    rules={[{ required: true, message: 'Please input your Location!' }]}
+                >
+                    <Input
+                        prefix={<EnvironmentOutlined className="site-form-item-icon" style={{ "margin": "0 0 0 1rem" }} />}
+                        placeholder="Grafton Street, Dublin"
+                        size="large"
+                        style={{ "borderRadius": "5rem", "margin": "0.5rem 0" }}
+                    />
+                </Form.Item>
+                <Form.Item
+                    name="address"
+                    rules={[{ required: true, message: 'Please input your Old Password!' }]}
+                >
+                    <Input
+                        size="large"
+                        prefix={<HomeOutlined className="site-form-item-icon" style={{ "margin": "0 1rem" }} />}
+                        placeholder="Username"
+                        style={{ "borderRadius": "5rem", "margin": "0.5rem 0" }}
+                    />
+                </Form.Item>
+                <Form.Item
+                    name="card"
+                    rules={[{ required: true, message: 'Please input your Card!' }]}
+                >
+                    <Input
+                        prefix={<CreditCardOutlined style={{ "margin": "0 1rem" }} />}
+                        placeholder="AB 23"
+                        size="large"
+                        style={{ "borderRadius": "5rem", "margin": "0.5rem 0" }}
+                    />
+                </Form.Item>
+                <Form.Item>
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        className="login-form-button"
+                        size="large"
+                        shape="round"
+                        block
+                        style={{ "margin": "0.5rem 0" }}
+                    >
+                        Save
+                    </Button>
+                </Form.Item>
+            </Form>
         </div>
     )
 }

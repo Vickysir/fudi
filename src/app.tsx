3d847d45a9decc56e9a9f2c0dccf8da64de2075c
Vickysir <460022058@qq.com>
2021-04-06 15:59:52 +0800

    fix font size

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2021-03-09 11:09:54
- * @LastEditTime: 2021-03-09 11:11:58
- * @LastEditors: your name
+ * @LastEditTime: 2021-04-06 15:50:28
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /fudi/src/app.tsx
  */
@@ -11,7 +11,7 @@ import { useViewport } from "./comps/useViewport";
 import Web from "./pages/web";
 import Mobile from "./pages/mobile";
 import { WebRoutes } from "./routes";
-// import "./styles/index.less"
+import "./styles/font.less"
 
 
 export const APP = () => {

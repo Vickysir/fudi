98a5d8af1c752a479c6f0ca7ba9052c8a972524a
Vickysir <460022058@qq.com>
2021-06-02 21:48:11 +0800

    add(Ordercomfirm):dom

--- src/pages/web/orderComfirm/index.tsx
+++ src/pages/web/orderComfirm/index.tsx
@@ -1,15 +1,19 @@
 import WebFooter from '@/pages/components/header/webFooter'
 import WebHeader from '@/pages/components/header/webHeader'
 import OrderDetailsList from '../../components/orderDetailsList'
-import { Button } from 'antd';
+import { Button, Form, Radio, Row } from 'antd';
 import { ArrowLeftOutlined } from '@ant-design/icons';
 
 import React, { useState } from 'react'
 import './index.less'
 import { APIGetCartList } from '@/pages/api/request';
+import TextArea from 'antd/lib/input/TextArea';
+import RoundButton from '@/pages/components/antd/button';
 
 const OrderComfirm = () => {
     const [refreshHeaderCart, setRefreshHeaderCart] = useState(0);
+    const [form] = Form.useForm();
+
 
     const fetchData = async () => {
         setRefreshHeaderCart(new Date().getTime())
@@ -32,6 +36,52 @@ const OrderComfirm = () => {
                     <div>
                         <h1>Confirm Order</h1>
                         <OrderDetailsList refreshHeader={fetchData} />
+                        <Form
+                            form={form}
+                            layout="vertical"
+                            initialValues={{ payment: "a" }}
+                            style={{ margin: "1rem 2rem" }}
+                        >
+
+                            <Form.Item noStyle>
+                                <div className="orderComfirm-wrap-body-form-title">notes</div>
+                            </Form.Item>
+                            <Form.Item
+                                name="notes"
+                                label="notes"
+                                noStyle
+                            >
+                                <TextArea />
+                            </Form.Item>
+                            <Form.Item noStyle>
+                                <div className="orderComfirm-wrap-body-form-title">Payment Method</div>
+                            </Form.Item>
+                            <Form.Item
+                                noStyle
+                                style={{ color: 'red' }}
+                            >
+                                <Row className="orderComfirm-wrap-body-form-payment">
+                                    <Form.Item noStyle name="payment">
+                                        <Radio.Group buttonStyle="solid">
+                                            <Radio.Button value="a">Cash</Radio.Button>
+                                            <Radio.Button value="b">Card</Radio.Button>
+                                        </Radio.Group>
+                                    </Form.Item>
+                                    <span>+ Add Voucher</span>
+                                </Row>
+                            </Form.Item>
+                            <Form.Item>
+                                <RoundButton
+                                    type="primary"
+                                    block
+                                    onClick={() => {
+                                    }}
+                                    style={{ marginTop: "4rem" }}
+                                >
+                                    Pay Now € 118
+                                </RoundButton>
+                            </Form.Item>
+                        </Form>
                     </div>
                     <div></div>
                 </div>

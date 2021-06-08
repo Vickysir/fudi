c64574d004c9d3fe46d79d038b3383eb47b1d579
Vickysir <460022058@qq.com>
2021-06-09 07:36:42 +0800

    fix

--- src/pages/web/orderComfirm/index.tsx
+++ src/pages/web/orderComfirm/index.tsx
@@ -77,6 +77,7 @@ const OrderComfirm = () => {
                                             <Radio.Button value="b">Card</Radio.Button>
                                         </Radio.Group>
                                     </Form.Item>
+                                    // TODO
                                     <span>+ Add Voucher</span>
                                 </Row>
                             </Form.Item>

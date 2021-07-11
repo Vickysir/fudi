b4adcef6829a462bb01913379fafd3597c96ca70
Vickysir <460022058@qq.com>
2021-07-11 14:16:59 +0000

    Deploying to gh-pages from  @ 09813320f5fa765c7fbf95712dac8af1abb4ec26 🚀

--- public_app_config.js
+++ public_app_config.js
@@ -5,9 +5,9 @@ const publicAppConfig = {
         api: "https://v2.api.dev.fudiandmore.ie/"
     },
     test: {
-        api: "http://app.fudiandmore.test.dev.51zhiyuan.net/"
+        api: "https://v2.api.dev.fudiandmore.ie/"
     },
     prod: {
-        api: "http://app.fudiandmore.test.dev.51zhiyuan.net/"
+        api: "https://v2.api.dev.fudiandmore.ie/"
     }
 }

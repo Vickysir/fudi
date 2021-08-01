b87c2afda31a7c774f6b29e6b2cc735a7b80a5dd
Vickysir <460022058@qq.com>
2021-08-01 23:24:23 +0800

    切换正式环境

--- public/public_app_config.js
+++ public/public_app_config.js
@@ -1,13 +1,15 @@
 const publicAppConfig = {
     common: {},
     dev: {
-        api: "http://app.fudiandmore.test.dev.51zhiyuan.net/"
-        // api: "https://v2.api.dev.fudiandmore.ie/"
+        // api: "http://app.fudiandmore.test.dev.51zhiyuan.net/"
+        api: "https://v2.api.dev.fudiandmore.ie/"
     },
     test: {
-        api: "http://app.fudiandmore.test.dev.51zhiyuan.net/"
+        // api: "http://app.fudiandmore.test.dev.51zhiyuan.net/"
+        api: "https://v2.api.dev.fudiandmore.ie/"
     },
     prod: {
-        api: "http://app.fudiandmore.test.dev.51zhiyuan.net/"
+        // api: "http://app.fudiandmore.test.dev.51zhiyuan.net/"
+        api: "https://v2.api.dev.fudiandmore.ie/"
     }
 }

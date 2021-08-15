cb7e0dbb040e506871877029bb099734886f2af3
Vickysir <460022058@qq.com>
2021-08-15 16:55:14 +0800

    fixup! update(docs):统计测试出现的问题

--- src/pages/web/payment/index.tsx
+++ src/pages/web/payment/index.tsx
@@ -1,7 +1,7 @@
 
 
 import React, { useEffect, useState } from 'react'
-import { Button, Form, Input, message, Radio, Row } from 'antd';
+import { Button, Form, Input, message, Radio, Row, Spin } from 'antd';
 import { ArrowLeftOutlined, UserOutlined } from '@ant-design/icons';
 import { useAppStore } from '@/__internal';
 import './index.less'
@@ -17,6 +17,8 @@ const Payment = (props) => {
     const commonInfo = useAppStore("commonInfo");
     const [submitValue, setSubmitValue] = useState<PaymentOnlineEntityPost>();
     const [publicKey, setPublicKey] = useState('');
+    const [loading, setLoading] = useState(true);
+
     const id = commonInfo?.userOrderId
 
 
@@ -41,6 +43,7 @@ const Payment = (props) => {
                         rsaKey: data?.rsaKey
                     })
                 }
+                setLoading(false);
             } catch (err) {
                 console.log(`APIPaymentEncryptRsa:`, err)
             }
@@ -73,106 +76,110 @@ const Payment = (props) => {
     }
     return (
         <div>
-            <div className="payment-wrap">
-                <div className="payment-wrap-header">
-                    <Button
-                        type="primary"
-                        shape="round"
-                        onClick={async () => {
-                            try {
-                                const { event, data } = await APIPaymentOnlineCheck({ userOrderId: id })
-                                if (event === 'SUCCESS' && data?.status === 0) {
-                                    history.go(-1);
-                                } else if (event === 'SUCCESS' && data?.status === 1) {
-                                    message.info("The order has been placed, place contact customer service for more info", 5);
-                                    history.push('/home');
-                                }
-                            }
-                            catch (err) {
-                                console.log(`APIPaymentOnlineCheck back:`, err)
-                            }
-                        }}
-                    >
-                        <ArrowLeftOutlined />Back
-                        </Button>
-                </div>
-                <div className="payment-wrap-body">
-                    <div>
-                        <h1>Payment</h1>
-                        <ul className="payment-wrap-body-cardInfo">
-                            <li>
-                                <p className="payment-wrap-body-cardInfo-title">Card Number</p>
-                                <RoundInput
-                                    placeholder="**** **** **** ****"
-                                    style={{ paddingLeft: "16px" }}
-                                    onChange={(e) => { handleChange(e, 'cardNumber') }}
-                                />
-                            </li>
-                            <li>
-                                <div>
-                                    <p className="payment-wrap-body-cardInfo-title">Expiry Date</p>
-                                    <RoundInput
-                                        placeholder="mm/yyyy"
-                                        style={{ paddingLeft: "16px" }}
-                                        onChange={(e) => { handleChange(e, 'expiryDate') }}
-                                    />
-                                </div>
-                                <div>
-                                    <p className="payment-wrap-body-cardInfo-title">CVV</p>
-                                    <RoundInput
-                                        placeholder="***"
-                                        style={{ paddingLeft: "16px" }}
-                                        onChange={(e) => { handleChange(e, 'cvc') }}
-
-                                    />
-                                </div>
-                            </li>
-                        </ul>
-                        <div>
-                            <RoundInput
-                                placeholder="Cardoldern Name"
-                                style={{ paddingLeft: "16px" }}
-                                prefix={<UserOutlined />}
-                                onChange={(e) => { handleChange(e, 'customerName') }}
-
-                            />
-                        </div>
-                        <div>
-                            <p>We accept Visa & Master Card</p>
-                        </div>
-
-                        <div>
-                            <RoundButton
-                                type="primary"
-                                block
-                                onClick={async () => {
-                                    try {
-                                        if (!submitValue) { return message.error("Place input pyayment information") }
-                                        if (!submitValue?.cardNumber) { return message.error("Place input Card Number information") }
-                                        if (!submitValue?.expiryDate) { return message.error("Place input Expiry date information") }
-                                        if (!submitValue?.cvc) { return message.error("Place input CVV information") }
-                                        if (!submitValue?.customerName) { return message.error("Place input Cardoldern Name information") }
-                                        if (!submitValue?.userOrderId) { return message.error("订单号获取失败") }
-                                        if (!submitValue?.rsaKey) { return message.error("rsa 获取失败") }
-                                        console.log(`submitValue`, submitValue)
-                                        // 提交下单
-                                        const { event } = await APIPaymentOnlineEntity(rsa_encrypt(submitValue))
-                                        if (event === 'SUCCESS') {
-                                            message.success("Ordered Successfully")
-                                            history.push('/home');
-                                        }
-                                    } catch (err) {
-                                        console.log(`APIPaymentOnlineEntity err`, err)
+            <Spin spinning={loading} size="large" tip="Loading...">
+                <div className="payment-wrap">
+                    <div className="payment-wrap-header">
+                        <Button
+                            type="primary"
+                            shape="round"
+                            onClick={async () => {
+                                try {
+                                    const { event, data } = await APIPaymentOnlineCheck({ userOrderId: id })
+                                    if (event === 'SUCCESS' && data?.status === 0) {
+                                        history.go(-1);
+                                    } else if (event === 'SUCCESS' && data?.status === 1) {
+                                        message.info("The order has been placed, place contact customer service for more info", 5);
+                                        history.push('/home');
                                     }
+                                }
+                                catch (err) {
+                                    console.log(`APIPaymentOnlineCheck back:`, err)
+                                }
+                            }}
+                        >
+                            <ArrowLeftOutlined />Back
+                        </Button>
+                    </div>
+                    <div className="payment-wrap-body">
+                        <div>
+                            <h1>Payment</h1>
+                            <ul className="payment-wrap-body-cardInfo">
+                                <li>
+                                    <p className="payment-wrap-body-cardInfo-title">Card Number</p>
+                                    <RoundInput
+                                        placeholder="**** **** **** ****"
+                                        style={{ paddingLeft: "16px" }}
+                                        onChange={(e) => { handleChange(e, 'cardNumber') }}
+                                    />
+                                </li>
+                                <li>
+                                    <div>
+                                        <p className="payment-wrap-body-cardInfo-title">Expiry Date</p>
+                                        <RoundInput
+                                            placeholder="mm/yyyy"
+                                            style={{ paddingLeft: "16px" }}
+                                            onChange={(e) => { handleChange(e, 'expiryDate') }}
+                                        />
+                                    </div>
+                                    <div>
+                                        <p className="payment-wrap-body-cardInfo-title">CVV</p>
+                                        <RoundInput
+                                            placeholder="***"
+                                            style={{ paddingLeft: "16px" }}
+                                            onChange={(e) => { handleChange(e, 'cvc') }}
 
-                                }}
-                            >
-                                Confirm
+                                        />
+                                    </div>
+                                </li>
+                            </ul>
+                            <div>
+                                <RoundInput
+                                    placeholder="Cardoldern Name"
+                                    style={{ paddingLeft: "16px" }}
+                                    prefix={<UserOutlined />}
+                                    onChange={(e) => { handleChange(e, 'customerName') }}
+
+                                />
+                            </div>
+                            <div>
+                                <p>We accept Visa & Master Card</p>
+                            </div>
+
+                            <div>
+                                <RoundButton
+                                    type="primary"
+                                    block
+                                    onClick={async () => {
+                                        setLoading(true);
+                                        try {
+                                            if (!submitValue) { return message.error("Place input pyayment information") }
+                                            if (!submitValue?.cardNumber) { return message.error("Place input Card Number information") }
+                                            if (!submitValue?.expiryDate) { return message.error("Place input Expiry date information") }
+                                            if (!submitValue?.cvc) { return message.error("Place input CVV information") }
+                                            if (!submitValue?.customerName) { return message.error("Place input Cardoldern Name information") }
+                                            if (!submitValue?.userOrderId) { return message.error("订单号获取失败") }
+                                            if (!submitValue?.rsaKey) { return message.error("rsa 获取失败") }
+                                            console.log(`submitValue`, submitValue)
+                                            // 提交下单
+                                            const { event } = await APIPaymentOnlineEntity(rsa_encrypt(submitValue))
+                                            if (event === 'SUCCESS') {
+                                                setLoading(false);
+                                                message.success("Ordered Successfully")
+                                                history.push('/home');
+                                            }
+                                        } catch (err) {
+                                            console.log(`APIPaymentOnlineEntity err`, err)
+                                        }
+
+                                    }}
+                                >
+                                    Confirm
                         </RoundButton>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </Spin>
         </div>
     )
 }
